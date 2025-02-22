7f429ebdab0dd25a140dbadb0a279cb9a02e19d0
anirin <atu8823atu@gmail.com>
2025-02-23 03:05:20 +0900

    基礎を完了

--- docker/nginx/site/index.php
+++ docker/nginx/site/index.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+// HTTPレスポンスヘッダーを設定
+header("HTTP/1.1 400 Bad Request");
+header("Content-Type: text/plain; charset=UTF-8");
+header("Content-Length: 12");
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+// ボディ部分を出力
+echo "Hello World\n";
+?>
