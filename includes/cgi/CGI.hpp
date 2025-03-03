433cf02c288fcdc01b7bc76c36382463c5131ca0
komeno <tkomeno@student.42tokyo.jp>
2025-03-03 19:13:32 +0900

    clean start

--- includes/cgi/CGI.hpp
+++ includes/cgi/CGI.hpp
@@ -8,36 +8,21 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#include <map>
-#include <vector>
-#include <cstring>
-
-#include "../http/HttpRequest.hpp"
-#include "../config/BaseConf.hpp"
-
 class CGI
 {
 private:
     std::string _scriptPath;
-    std::string _method;
+
+    void createPipe(int pipefd[2]);
+    pid_t createChildProcess();
+    void executeScriptInChild(int pipefd[2]);
+    std::string readScriptOutput(int pipefd[2]);
 
 public:
     CGI(const std::string& scriptPath);
-
-    CGI(const std::string& scriptPath, const std::string& method);
-
-    std::string execute(const HttpRequest *request, const conf_value_t& conf_value);
-
     ~CGI();
 
-private:
-    void createPipe(int pipefd[2]);
-    pid_t createChildProcess();
-    void executeScriptInChild(int pipefd[2], char **envp);
-    std::string readScriptOutput(int pipefd[2]);
-    std::string parseOutput(const std::string& rawOutput);
-
-    std::string headerNameToEnvFormat(const std::string& headerName);
+    std::string execute();
 };
 
 #endif
--- src/cgi/CGI.cpp
+++ src/cgi/CGI.cpp
@@ -1,10 +1,6 @@
 #include "CGI.hpp"
 
-CGI::CGI(const std::string &scriptPath)
-    : _scriptPath(scriptPath), _method("GET") {}
-
-CGI::CGI(const std::string& scriptPath, const std::string& method) 
-    : _scriptPath(scriptPath), _method(method) {}
+CGI::CGI(const std::string &scriptPath) : _scriptPath(scriptPath) {}
 
 void CGI::createPipe(int pipefd[2]) {
     if (pipe(pipefd) == -1)
@@ -18,13 +14,13 @@ pid_t CGI::createChildProcess() {
     return pid;
 }
 
-void CGI::executeScriptInChild(int pipefd[2], char **envp) {
+void CGI::executeScriptInChild(int pipefd[2]) {
     close(pipefd[0]);
     dup2(pipefd[1], STDOUT_FILENO);
     close(pipefd[1]);
 
     char* args[] = {(char*)"/usr/bin/php", (char*)_scriptPath.c_str(), NULL};
-    execve(args[0], args, envp);
+    execve(args[0], args, NULL);
 
     throw std::runtime_error("execve");
 }
@@ -44,72 +40,16 @@ std::string CGI::readScriptOutput(int pipefd[2]) {
     return output;
 }
 
-/*
-    Produces: Content-Type -> CONTENT_TYPE
-*/
-std::string CGI::headerNameToEnvFormat(const std::string &headerName) {
-    std::string result;
-
-    for (size_t i = 0; i < headerName.size(); ++i) {
-        char c = headerName[i];
-        if (c == '-')
-            result += '_';
-        else
-            result += std::toupper(c);
-    }
-
-    return result;
-}
-
-std::string CGI::parseOutput(const std::string& rawOutput) {
-    size_t headerEnd = rawOutput.find("\r\n\r\n");
-    if (headerEnd == std::string::npos) {
-        return "Content-Type: text/html\r\n\r\n" + rawOutput;
-    }
-    
-    return rawOutput;
-}
-
-std::string CGI::execute(const HttpRequest *request, const conf_value_t& conf_value) {
+std::string CGI::execute() {
     int pipefd[2];
     pid_t pid;
     std::string output;
-
-    std::map<std::string, std::string> env;
-
-    env["REQUEST_METHOD"] = _method;
-    env["CONTENT_TYPE"] = request->getHeader().count("Content-Type") ? request->getHeader().at("Content-Type") : "";
-    env["CONTENT_LENGTH"] = request->getHeader().count("Content-Length") ? request->getHeader().at("Content-Length") : "";
-
-    env["SERVER_NAME"] = conf_value._server_name;
-    env["SERVER_PROTOCOL"] = "HTTP/1.1";
-    env["SERVER_PORT"] = conf_value._listen;
-
-    for (std::map<std::string, std::string>::const_iterator it = request->getHeader().begin(); 
-    it != request->getHeader().end(); ++it) {
-        if (it->first != "Content-Type" && it->first != "Content-Length") {
-       env["HTTP_" + headerNameToEnvFormat(it->first)] = it->second;
-        }
-    }
-
-    std::vector<std::string> envStrings;
-    for (std::map<std::string, std::string>::const_iterator it = env.begin(); 
-         it != env.end(); ++it) {
-        envStrings.push_back(it->first + "=" + it->second);
-    }
-    
-    char** envp = new char*[envStrings.size() + 1];
-    for (size_t i = 0; i < envStrings.size(); ++i) {
-        envp[i] = new char[envStrings[i].size() + 1];
-        strcpy(envp[i], envStrings[i].c_str());
-    }
-    envp[envStrings.size()] = NULL;
 
     try {
         createPipe(pipefd);
         pid = CGI::createChildProcess();
         if (pid == 0) {
-            CGI::executeScriptInChild(pipefd, envp);
+            CGI::executeScriptInChild(pipefd);
         } else {
             output = CGI::readScriptOutput(pipefd);
             waitpid(pid, NULL, 0);
@@ -119,11 +59,6 @@ std::string CGI::execute(const HttpRequest *request, const conf_value_t& conf_va
             _exit(1); 
         throw;
     }
-
-    for (size_t i = 0; i < envStrings.size(); ++i) {
-        delete[] envp[i];
-    }
-    delete[] envp;
 
     return output;
 }
